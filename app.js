2aa88227a5bb06b5d3e317b9b810dd801f870d6c
Brandon Fernández <andreyfdez18@gmail.com>
2021-02-13 01:51:44 +0000

    Feeling confident

---
+++ app.js
@@ -0,0 +1,18 @@
+  
+let pronoun = ['the', 'our', 'mine', 'us', 'his'];
+let adj = ['great', 'big', 'small','most','creative'];
+let noun = ['jogger', 'racoon','agency','yoda','developer'];
+let extention = ['.com', '.net', '.us', '.io'];
+
+
+
+    pronoun.forEach(item1 => {
+        adj.forEach(item2 => {
+            noun.forEach(item3 => {
+                extention.forEach(item4 =>{
+                     console.log(item1.concat(item2.concat(item3.concat(item4))));
+                })
+                
+            });            
+        });
+    });
